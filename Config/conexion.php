983de96712c1e80c07243dc3041166ea4f20aa6b
Samuel Alvarado <64099845+Samu-hash@users.noreply.github.com>
2024-10-25 23:35:12 -0600

    Update conexion.php 3

--- Config/conexion.php
+++ Config/conexion.php
@@ -7,6 +7,8 @@ class Database {
         try{
             $this->mysql = $this->conectar();
             //echo 'Conexión exitosa';
+
+            $this->crearTabla();
         } catch(PDOException $exepcion){
             echo 'Error de conexión' . $exepcion->getMessage();
         }
@@ -28,15 +30,27 @@ class Database {
         return $pdo;
     }
 
+     private $id;
+    private $solicitante;
+    private $telefono;
+    private $email;
+    private $servicio;
+    private $profesional;
+    private $fecha;
+    private $solicitud;
+
     private function crearTabla() {
         $sql = "
             CREATE TABLE IF NOT EXISTS citas (
-                id INT PRIMARY KEY,
-                nombre VARCHAR(100) NOT NULL,
-                fecha DATE NOT NULL,
-                hora TIME NOT NULL,
-                descripcion TEXT
-            )
+            id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            solicitante VARCHAR(100) NOT NULL,
+            telefono VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            servicio VARCHAR(100) NOT NULL,
+            profesional VARCHAR(100) NOT NULL,
+            fecha DATE NOT NULL,
+            solicitud VARCHAR(100) NOT NULL
+        )
         ";
 
         $this->mysql->exec($sql);
