5538754b2ead0edccfe6965a20ca9254530efede
Samuel Alvarado <64099845+Samu-hash@users.noreply.github.com>
2024-10-25 23:32:57 -0600

    Update conexion.php 2

--- Config/conexion.php
+++ Config/conexion.php
@@ -15,10 +15,10 @@ class Database {
     public static function conectar () {
 
         $dd = 'Database=gestion;Server=pg-server01.postgres.database.azure.com;User Id=kwkeweooph;Password=36gi0OagColj$E$f'
-        $hostname = 'us-cdbr-east-05.cleardb.net';
-        $database = 'heroku_734ef63ccdc2de6';
-        $username = 'b42d821ea516bb';
-        $password = '33aa38ba';
+        $hostname = 'pg-server01.postgres.database.azure.com';
+        $database = 'gestion';
+        $username = 'kwkeweooph';
+        $password = '36gi0OagColj$E$f';
         $charset = 'utf-8';
 
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
@@ -26,6 +26,20 @@ class Database {
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         return $pdo;
+    }
+
+    private function crearTabla() {
+        $sql = "
+            CREATE TABLE IF NOT EXISTS citas (
+                id INT PRIMARY KEY,
+                nombre VARCHAR(100) NOT NULL,
+                fecha DATE NOT NULL,
+                hora TIME NOT NULL,
+                descripcion TEXT
+            )
+        ";
+
+        $this->mysql->exec($sql);
     }
 }
 
